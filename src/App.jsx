f52d2a721552044fbee490cffdc81a9d56780d0e
Mohd Aman Azam <amancode006@gmail.com>
2024-03-07 19:51:05 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./Pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  console.log("link-->", window.location.href);
   let str = window.location.href.split("/").reverse()[0];
   // let str = window.location.href;
   console.log("str5-->", str);
