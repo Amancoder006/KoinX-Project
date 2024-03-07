f80722bf4a562b19c621ec4ca3a766c5f9cb65fc
Mohd Aman Azam <amancode006@gmail.com>
2024-03-07 19:55:26 +0530

    link4

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ function App() {
   console.log("link-->", window.location.href);
   let str = window.location.href.split("/").reverse()[0];
   // let str = window.location.href;
-  console.log("str5-->", str);
+  console.log("str9-->", str);
   return (
     <BrowserRouter>
       <Routes>
         <Route path={`/${str}`} element={<Home coin={str} />} />
         <Route path={`/bitcoin`} element={<Home coin={"bitcoin"} />} />
+        <Route path={`/ethereum`} element={<Home coin={"ethereum"} />} />
       </Routes>
     </BrowserRouter>
   );
