f2588a4c247b56cd9c2df6ccab79b5b3fd95fafe
Mohd Aman Azam <amancode006@gmail.com>
2024-03-07 19:59:10 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Home from "./Pages/Home.jsx";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path={`/`} element={<Home />} />
+      <Route path={`/bitcoin`} element={<Home coin={"bitcoin"} />} />
+      <Route path={`/ethereum`} element={<Home coin={"ethereum"} />} />
+    </Routes>
+  </BrowserRouter>
+);
